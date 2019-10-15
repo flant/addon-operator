a4e950521680e5d9ad0a6bbe56a16c39f42e131c
Ivan Mikheykin <ivan.mikheykin@flant.com>
2019-10-15 12:32:57 +0300

    fix: run afterDeleteHelm hooks for modules that should be deleted on startup

--- pkg/addon-operator/operator_test.go
+++ pkg/addon-operator/operator_test.go
@@ -62,6 +62,8 @@ type ModuleManagerMock struct {
 	ScheduledHookErrorsCount int
 }
 
+var _ module_manager.ModuleManager = &ModuleManagerMock{}
+
 var mainTestGlobalHooksMap = map[module_manager.BindingType][]string{
 	module_manager.OnStartup: {
 		"hook_1__31", "hook_2__32",
@@ -244,6 +246,10 @@ func (m *ModuleManagerMock) RunModuleHook(hookName string, binding module_manage
 		m.ScheduledHookErrorsCount--
 		return fmt.Errorf("fake module hook error: /bin/ash not found")
 	}
+	return nil
+}
+
+func (m *ModuleManagerMock) InitModuleHooks(module *module_manager.Module) error {
 	return nil
 }
 
