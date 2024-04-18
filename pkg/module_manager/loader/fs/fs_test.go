7f943b911f7a981c600aeccff02853f6a51b9927
Daniil Stepanenko <daniil.stepanenko@flant.com>
2024-04-18 16:31:10 +0300

    WIP
    Signed-off-by: Daniil Stepanenko <daniil.stepanenko@flant.com>

--- pkg/module_manager/loader/fs/fs_test.go
+++ pkg/module_manager/loader/fs/fs_test.go
@@ -8,8 +8,6 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-
-	"github.com/flant/addon-operator/pkg/values/validation"
 )
 
 func TestNewFileSystemLoader(t *testing.T) {
@@ -45,9 +43,7 @@ fooBar:
 `), 0o666)
 	require.NoError(t, err)
 
-	vv := validation.NewValuesValidator()
-
-	loader := NewFileSystemLoader(filepath.Join(tmpDir, "modules"), vv)
+	loader := NewFileSystemLoader(filepath.Join(tmpDir, "modules"))
 	modules, err := loader.LoadModules()
 	require.NoError(t, err)
 	m := modules[0]
@@ -63,7 +59,7 @@ func TestDirWithSymlinks(t *testing.T) {
 	g := NewWithT(t)
 	dir := "testdata/module_loader/dir1"
 
-	ld := NewFileSystemLoader(dir, validation.NewValuesValidator())
+	ld := NewFileSystemLoader(dir)
 
 	mods, err := ld.LoadModules()
 	g.Expect(err).ShouldNot(HaveOccurred())
@@ -75,7 +71,7 @@ func TestLoadMultiDir(t *testing.T) {
 	g := NewWithT(t)
 	dirs := "testdata/module_loader/dir2:testdata/module_loader/dir3"
 
-	ld := NewFileSystemLoader(dirs, validation.NewValuesValidator())
+	ld := NewFileSystemLoader(dirs)
 
 	mods, err := ld.LoadModules()
 	g.Expect(err).ShouldNot(HaveOccurred())
