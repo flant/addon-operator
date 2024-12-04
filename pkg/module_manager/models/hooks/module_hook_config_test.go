859d7a5f58d0b94aa8039cfddb3c6c90b8c776b9
Pavel Okhlopkov <pavel.okhlopkov@flant.com>
2024-12-04 10:42:12 +0300

    fix tests
    Signed-off-by: Pavel Okhlopkov <pavel.okhlopkov@flant.com>

--- pkg/module_manager/models/hooks/module_hook_config_test.go
+++ pkg/module_manager/models/hooks/module_hook_config_test.go
@@ -3,6 +3,9 @@ package hooks
 import (
 	"testing"
 
+	sdkhook "github.com/deckhouse/module-sdk/pkg/hook"
+	"github.com/deckhouse/module-sdk/pkg/utils/ptr"
+
 	. "github.com/onsi/gomega"
 
 	. "github.com/flant/addon-operator/pkg/hook/types"
@@ -135,20 +138,29 @@ func Test_ModuleHook_Batch_Config_v0_v1(t *testing.T) {
 	var config *ModuleHookConfig
 
 	tests := []struct {
-		name       string
-		hookName   string
-		configText string
-		assertion  func()
+		name      string
+		hookName  string
+		config    *sdkhook.HookConfig
+		assertion func()
 	}{
 		{
 			"load v0 module config",
 			"hook_v0",
-			`{"onStartup":10,
-               "schedule":[{"crontab":"*/5 * * * * *"}],
-               "beforeHelm": 5,
-               "afterHelm": 15,
-               "afterDeleteHelm": 25
-             }`,
+			// `{"onStartup":10,
+			//    "schedule":[{"crontab":"*/5 * * * * *"}],
+			//    "beforeHelm": 5,
+			//    "afterHelm": 15,
+			//    "afterDeleteHelm": 25
+			//  }`,
+			&sdkhook.HookConfig{
+				OnStartup: ptr.To(uint(10)),
+				Schedule: []sdkhook.ScheduleConfig{
+					{Crontab: "*/5 * * * * *"},
+				},
+				OnBeforeHelm:      ptr.To(uint(5)),
+				OnAfterHelm:       ptr.To(uint(15)),
+				OnAfterDeleteHelm: ptr.To(uint(25)),
+			},
 			func() {
 				g.Expect(err).ShouldNot(HaveOccurred())
 				g.Expect(config.Schedules).To(HaveLen(1))
@@ -167,7 +179,10 @@ func Test_ModuleHook_Batch_Config_v0_v1(t *testing.T) {
 		{
 			"load v0 onStart",
 			"hook_v0",
-			`{"onStartup":10}`,
+			// `{"onStartup":10}`,
+			&sdkhook.HookConfig{
+				OnStartup: ptr.To(uint(10)),
+			},
 			func() {
 				g.Expect(err).ShouldNot(HaveOccurred())
 				g.Expect(config.Bindings()).To(HaveLen(1))
@@ -176,12 +191,22 @@ func Test_ModuleHook_Batch_Config_v0_v1(t *testing.T) {
 		{
 			"load v1 module config",
 			"hook_v1",
-			`{"configVersion": "v1",
-                  "onStartup":10,
-                  "kubernetes":[{"kind":"Pod", "watchEvent":["Added"]}],
-                 "beforeHelm": 98,
-                 "afterHelm": 58,
-                 "afterDeleteHelm": 18}`,
+			// `{"configVersion": "v1",
+			//       "onStartup":10,
+			//       "kubernetes":[{"kind":"Pod", "watchEvent":["Added"]}],
+			//      "beforeHelm": 98,
+			//      "afterHelm": 58,
+			//      "afterDeleteHelm": 18}`,
+			&sdkhook.HookConfig{
+				ConfigVersion: "v1",
+				OnStartup:     ptr.To(uint(10)),
+				Kubernetes: []sdkhook.KubernetesConfig{
+					{Kind: "Pod"},
+				},
+				OnBeforeHelm:      ptr.To(uint(98)),
+				OnAfterHelm:       ptr.To(uint(58)),
+				OnAfterDeleteHelm: ptr.To(uint(18)),
+			},
 			func() {
 				g.Expect(err).ShouldNot(HaveOccurred())
 				g.Expect(config.HasBinding(OnStartup)).To(BeTrue())
@@ -200,16 +225,28 @@ func Test_ModuleHook_Batch_Config_v0_v1(t *testing.T) {
 		{
 			"load v1 module yaml config",
 			"hook_v1",
-			`
-configVersion: v1
-onStartup: 10
-kubernetes:
-- kind: Pod
-  watchEvent: ["Added"]
-beforeHelm: 98
-afterHelm: 58
-afterDeleteHelm: 18
-`,
+			//	`
+			//
+			// configVersion: v1
+			// onStartup: 10
+			// kubernetes:
+			//   - kind: Pod
+			//     watchEvent: ["Added"]
+			//
+			// beforeHelm: 98
+			// afterHelm: 58
+			// afterDeleteHelm: 18
+			// `,
+			&sdkhook.HookConfig{
+				ConfigVersion: "v1",
+				OnStartup:     ptr.To(uint(10)),
+				Kubernetes: []sdkhook.KubernetesConfig{
+					{Kind: "Pod"},
+				},
+				OnBeforeHelm:      ptr.To(uint(98)),
+				OnAfterHelm:       ptr.To(uint(58)),
+				OnAfterDeleteHelm: ptr.To(uint(18)),
+			},
 			func() {
 				g.Expect(err).ShouldNot(HaveOccurred())
 				g.Expect(config.HasBinding(OnStartup)).To(BeTrue())
@@ -225,25 +262,13 @@ afterDeleteHelm: 18
 				g.Expect(config.AfterDeleteHelm.Order).To(Equal(18.0))
 			},
 		},
-		{
-			"load v1 bad module config",
-			"hook_v1",
-			`{"configVersion": "v1", "onStartuppp":10, "kubernetes":[{"kind":"Pod", "watchEvent":["Added"]}], "beforeHelm":98}`,
-			func() {
-				g.Expect(err).Should(HaveOccurred())
-				g.Expect(err.Error()).Should(And(
-					ContainSubstring("1 error occurred"),
-					ContainSubstring("onStartuppp is a forbidden property"),
-				))
-			},
-		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			g = NewWithT(t)
 			config = &ModuleHookConfig{}
-			err = config.LoadAndValidateBatchConfig(nil)
+			err = config.LoadAndValidateBatchConfig(tt.config)
 			tt.assertion()
 		})
 	}
