a60415be6d3786e3c91e0d467f920dac3b47c84a
Pavel Okhlopkov <pavel.okhlopkov@flant.com>
2025-01-28 18:16:47 +0300

    lint
    Signed-off-by: Pavel Okhlopkov <pavel.okhlopkov@flant.com>

--- pkg/helm/helm3lib/helm3lib.go
+++ pkg/helm/helm3lib/helm3lib.go
@@ -13,9 +13,6 @@ import (
 
 	"github.com/deckhouse/deckhouse/pkg/log"
 	logContext "github.com/deckhouse/deckhouse/pkg/log/context"
-	"github.com/flant/addon-operator/pkg/helm/client"
-	"github.com/flant/addon-operator/pkg/helm/post_renderer"
-	"github.com/flant/addon-operator/pkg/utils"
 	"helm.sh/helm/v3/pkg/action"
 	"helm.sh/helm/v3/pkg/chart/loader"
 	"helm.sh/helm/v3/pkg/chartutil"
@@ -28,6 +25,10 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/client-go/rest"
+
+	"github.com/flant/addon-operator/pkg/helm/client"
+	"github.com/flant/addon-operator/pkg/helm/post_renderer"
+	"github.com/flant/addon-operator/pkg/utils"
 )
 
 var helmPostRenderer *post_renderer.PostRenderer
