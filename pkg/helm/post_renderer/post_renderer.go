2755a8d3ac4348475c1e52af64786a4e9fc27a65
Evsyukov Denis <denis.evsyukov@flant.com>
2025-05-23 15:40:13 +0300

    fix: enhance label handling in PostRenderer by using maps.Copy for extra labels
    Signed-off-by: Evsyukov Denis <denis.evsyukov@flant.com>

--- pkg/helm/post_renderer/post_renderer.go
+++ pkg/helm/post_renderer/post_renderer.go
@@ -1,22 +1,27 @@
+// Package post_renderer provides a Helm post-renderer for adding extra labels to manifests.
 package post_renderer
 
 import (
 	"bytes"
 	"fmt"
+	"maps"
 
 	"sigs.k8s.io/kustomize/kyaml/kio"
 )
 
+// PostRenderer adds extra labels to rendered Kubernetes manifests.
 type PostRenderer struct {
 	extraLabels map[string]string
 }
 
+// NewPostRenderer creates a new PostRenderer with the given extra labels.
 func NewPostRenderer(extraLabels map[string]string) *PostRenderer {
 	return &PostRenderer{
 		extraLabels: extraLabels,
 	}
 }
 
+// Run adds extra labels to all resources in the rendered manifests.
 func (p *PostRenderer) Run(renderedManifests *bytes.Buffer) (*bytes.Buffer, error) {
 	if len(p.extraLabels) == 0 {
 		return renderedManifests, nil
@@ -29,9 +34,10 @@ func (p *PostRenderer) Run(renderedManifests *bytes.Buffer) (*bytes.Buffer, erro
 
 	for _, node := range nodes {
 		labels := node.GetLabels()
-		for k, v := range p.extraLabels {
-			labels[k] = v
+		if labels == nil {
+			labels = make(map[string]string)
 		}
+		maps.Copy(labels, p.extraLabels)
 
 		_ = node.SetLabels(labels)
 	}
