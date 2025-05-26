10b3581d63d480f84802d8a4e8337c87d0c1461e
Pavel Okhlopkov <pavel.okhlopkov@flant.com>
2025-05-26 13:04:08 +0300

    fix example
    Signed-off-by: Pavel Okhlopkov <pavel.okhlopkov@flant.com>

--- examples/700-go-hook/modules/001-module-go-hooks/hooks/go_hooks.go
+++ examples/700-go-hook/modules/001-module-go-hooks/hooks/go_hooks.go
@@ -54,7 +54,9 @@ func ObjFilter(obj *unstructured.Unstructured) (gohook.FilterResult, error) {
 }
 
 func run(input *gohook.HookInput) error {
-	for _, o := range input.NewSnapshots.Get("pods") {
+	pods := input.NewSnapshots.Get("pods")
+
+	for _, o := range pods {
 		podSpec := new(podSpecFilteredObj)
 
 		err := o.UnmarshalTo(podSpec)
@@ -66,8 +68,8 @@ func run(input *gohook.HookInput) error {
 			slog.String("spec", fmt.Sprintf("%+v", podSpec)))
 	}
 
-	input.Logger.Info("Hello from on_kube.pods2! I have snapshots",
-		slog.Int("count", len(input.Snapshots)))
+	input.Logger.Info("Hello from on_kube.pods2! I have pods in snapshots",
+		slog.Int("count", len(pods)))
 
 	input.MetricsCollector.Add("addon_go_hooks_total", 1.0, nil)
 
