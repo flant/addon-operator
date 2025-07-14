ce81de2b86024698fdf15b919612fe0fed7ff319
Pavel Okhlopkov <pavel.okhlopkov@flant.com>
2025-07-14 13:19:53 +0300

    fix
    Signed-off-by: Pavel Okhlopkov <pavel.okhlopkov@flant.com>

--- pkg/module_manager/models/hooks/kind/shellhook.go
+++ pkg/module_manager/models/hooks/kind/shellhook.go
@@ -192,7 +192,7 @@ func (sh *ShellHook) Execute(ctx context.Context, configVersion string, bContext
 		return result, fmt.Errorf("got bad json patch for values: %w", err)
 	}
 
-	result.Metrics, err = metricoperation.MetricOperationsFromFile(metricsPath)
+	result.Metrics, err = metricoperation.MetricOperationsFromFile(metricsPath, h.GetName())
 	if err != nil {
 		return result, fmt.Errorf("got bad metrics: %w", err)
 	}
