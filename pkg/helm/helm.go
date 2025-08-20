af5bc1cfa51e3f268adecedbb158cda581880bab
Evsyukov Denis <denis.evsyukov@flant.com>
2025-08-20 16:00:17 +0300

    fix: comment out werf.io/track-termination-mode annotation for nelm
    Signed-off-by: Evsyukov Denis <denis.evsyukov@flant.com>

--- pkg/helm/helm.go
+++ pkg/helm/helm.go
@@ -46,8 +46,8 @@ func (f *ClientFactory) NewClient(logger *log.Logger, options ...ClientOption) c
 		// Add werf.io/skip-logs and werf.io/track-termination-mode annotations when using nelm
 		if f.ClientType == Nelm {
 			c.WithExtraAnnotations(map[string]string{
-				"werf.io/skip-logs":              "true",
-				"werf.io/track-termination-mode": "NonBlocking",
+				"werf.io/skip-logs": "true",
+				// "werf.io/track-termination-mode": "NonBlocking",
 			})
 		}
 
