2a85734289fa7c93c2a924ef764efe11c9304c66
Pavel Okhlopkov <pavel.okhlopkov@flant.com>
2026-01-14 17:33:30 +0300

    fix nelm
    Signed-off-by: Pavel Okhlopkov <pavel.okhlopkov@flant.com>

--- pkg/helm/nelm/nelm.go
+++ pkg/helm/nelm/nelm.go
@@ -339,8 +339,9 @@ func (c *NelmClient) UpgradeRelease(releaseName, modulePath string, valuesPaths 
 			ValuesSet:   setValues,
 		},
 		TrackingOptions: common.TrackingOptions{
-			NoPodLogs:       true,
-			NoFinalTracking: true,
+			NoPodLogs:                    true,
+			NoFinalTracking:              true,
+			LegacyHelmCompatibleTracking: true,
 		},
 		Chart:                    modulePath,
 		DefaultChartName:         releaseName,
@@ -436,8 +437,9 @@ func (c *NelmClient) DeleteRelease(releaseName string) error {
 			KubeContextCurrent: c.opts.KubeContext,
 		},
 		TrackingOptions: common.TrackingOptions{
-			NoPodLogs:       true,
-			NoFinalTracking: true,
+			NoPodLogs:                    true,
+			NoFinalTracking:              true,
+			LegacyHelmCompatibleTracking: true,
 		},
 		ReleaseHistoryLimit:      int(c.opts.HistoryMax),
 		DefaultDeletePropagation: "Background",
